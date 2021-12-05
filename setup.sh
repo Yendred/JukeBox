6df01dd629ab161575270b5a41e8fbd2687e16f9
henry Thompson <henry.thompson@gmail.com>
2021-12-05 11:20:43 -0500

    naming and error description changes, new requirements file

--- setup.sh
+++ setup.sh
@@ -4,134 +4,93 @@ cd ~
 #update
 sudo apt -y update
 
-# Install SSH Server
-sudo apt -y install openssh-server
-
-# Install USB Auto Mount
-sudo apt -y install usbmount
-# May need to edit    /etc/usbmount/usbmount.conf
-#   ls -l /dev/disk/by-uuid/
-#       1C87-CC63
-
-# create folder to mount the USB (Music) drive to
-#   sudo mkdir -p /mnt/usb1
-#give it the appropiate permissions
-#   sudo chown -R pi:pi /mnt/usb1
-
-# Edit /etc/fstab
-# https://www.shellhacks.com/raspberry-pi-mount-usb-drive-automatically/
-#   Get UUID and FileSystem Type
-#       lsblk -fp
-#           NAME             FSTYPE LABEL  UUID                                 FSAVAIL FSUSE% MOUNTPOINT
-#           /dev/sda
-#           └─/dev/sda1      exfat  Music  1C87-CC63                              56.6G    53% /mnt/USBMusic
-#
-#
-#   sudo blkid
-#       /dev/sda1: LABEL="Music" UUID="1C87-CC63" TYPE="exfat" PARTUUID="44e444a8-01"
-
-
-#   UUID=1C87-CC63 /mnt/USBMusic exfat defaults,auto,users,rw,nofail 0 0
-
-# open the firewall to allow SSH
-sudo ufw allow ssh
-
-# Check the status of the SSH Server
-#sudo systemctl status ssh
-
 # Install GIT
-sudo apt -y install wget, git
-git config --global user.email "henry.thompson@gmail.com"
-git config --global user.name "henry Thompson"
+sudo apt -y install git
+#git config --global user.email "My Email Address"
+#git config --global user.name "My Name"
+
+mkdir ~/src
+cd ~/src
+git clone https://github.com/Yendred/JukeBox.git
+
+# create directory for logs
+sudo mkdir /var/log/jukebox
+sudo chown pi:pi /var/log/jukebox
 
 # alsa-base:
 # alsa-utils:
 # pulseaudio:
-sudo apt -y install alsa-base alsa-utils pulseaudio
+sudo apt -y install alsa-base alsa-utils
+# sudo apt -y install ffmpeg
+sudo apt -y install mpd mpc
+sudo apt -y purge pulseaudio
 
-# ffmpeg:
-# mpd:
-# mpc:
-# sudo apt -y install ffmpeg mpd mpc zip
-sudo apt -y install ffmpeg mpd mpc
-
-# create folder for logs
-sudo mkdir /var/log/musiccards
-sudo chown pi:pi musiccards/
-
-
-# Install youtube-dl
-sudo wget https://yt-dl.org/latest/youtube-dl -O /usr/local/bin/youtube-dl
-sudo chmod a+rx /usr/local/bin/youtube-dl
-
+# install python 3 and pip
 sudo apt -y install python3 python3-pip
 
-# Install youtube-dl python module
-sudo -H pip3 install --upgrade youtube-dl
-sudo -H pip3 install --upgrade python-mpd2
-sudo -H pip3 install --upgrade evdev
+# install exfat, this is the USB Drive format
+sudo apt install -y exfat-fuse
 
 sudo apt -y autoremove
 sudo apt -y clean
+
+# edit /etc/mpd.conf and make the folowing changes
+# comment out this line
+#       bind_to_address         "localhost"
+#
+# uncomment this line
+#       bind_to_address         "/run/mpd/socket"
+# sed -i -e 's/#bind_to_address "/run/mpd/socket"/bind_to_address "/run/mpd/socket"/' /etc/mpd.conf
+#
+# uncomment this line and change replace "hardware" with "software"
+#       mixer_type              "hardware"      # optional
+#
+# uncomment this line and change replace "no" with "yes"
+#       volume_normalization    "no"
+
+
+
+cat /proc/asound/cards
+# if the output of the above command does not show the speakers as the
+# default or 0 device we will need to edit /usr/share/alsa/alsa.conf and change the following
+# find the following lines and change the number to the device as shown in 'cat /proc/asound/cards'
+#   defaults.pcm.card 1
+#   defaults.ctl.card 1
+# I dont like this solution, if the devices switch places we will no longer have a working sound device
+
+
+# Install python modules
+# sudo -H pip3 install --upgrade python-mpd2
+# sudo -H pip3 install --upgrade evdev
+/usr/bin/python3 -m pip install --user --upgrade python-mpd2 evdev jsonpath-ng black
+
+# /usr/bin/env python3 -m pip freeze > requirements.txt
+# /usr/bin/env python3 -m pip install -r requirements.txt
+
+# https://www.shellhacks.com/raspberry-pi-mount-usb-drive-automatically/
+# create mount point for USB Drive
+sudo mkdir /mnt/USBMusic
+
+# run 'lsblk -fp'  to determine where the USB Drive is mounted, on this run it is located at /dev/sda1
+sudo mount -t exfat /dev/sda1 /mnt/USBMusic
+
+# run 'blkid' to determine the UUID, on this run it is '1C87-CC63'
+#/dev/sda1: LABEL="Music" UUID="1C87-CC63" BLOCK_SIZE="512" TYPE="exfat" PARTUUID="44e444a8-01"
+
+# add the following to /etc/fstab
+# Remember to change the UUID and mount location with what was determined above '/mnt/USBMusic'
+#   UUID=1C87-CC63 /mnt/USBMusic exfat defaults,auto,users,rw,nofail 0 0
+
+
 
 # on some systems the user is not in the input group, let's make sure they are
 # This is needed if the user wants to use the RFID input device
 sudo usermod -a -G input $USER
 sudo usermod -a -G sudo $USER
 
-# cd $HOME/src/music-cards/src
-# sudo cp musiccards.service /etc/systemd/system/musiccards.service
-# sudo chmod 644 /etc/systemd/system/musiccards.service
-# sudo systemctl daemon-reload
-# sudo systemctl start musiccards.service
-# sudo systemctl enable musiccards.service
-
-# Could not get bluetooth working
-# # Turn on BlueTooth
-# # unmask the Bluetooth service
-# sudo systemctl unmask bluetooth
-# sudo systemctl enable bluetooth
-# # the bluetooth module was not loading so you need to figure that out, but manually you can type
-# sudo modprobe btusb
-# # Start the Bluetooth service
-# sudo systemctl start bluetooth
-
-# #install Bluetooth command line tools, you should reboot after installing these
-# sudo apt install bluez
-# sudo apt install pi-bluetooth
-# sudo apt install pulseaudio-module-bluetooth
-# pulseaudio -k
-# pulseaudio --start
-
-# # add user to the bluetooth users group
-# sudo usermod -a -G bluetooth $USER
-
-# # interactive mode
-# sudo bluetoothctl << EOF
-# power on
-# agent on
-# default-agent
-# pair D8:37:3B:63:1B:30
-# trust D8:37:3B:63:1B:30
-# connect D8:37:3B:63:1B:30
-# EOF
-
-# sudo bluetoothctl << EOF
-# connect D8:37:3B:63:1B:30
-# EOF
-
-
-
-# # sudo bluetoothctl
-# # If you dont get a result from show you have an issue
-# # --> show
-# #  https://www.youtube.com/watch?v=lHwvoFLbAkM
-# #  https://forums.raspberrypi.com/viewtopic.php?t=242281
-# #  https://forums.raspberrypi.com/viewtopic.php?t=207025
-# #  https://raspberrypi.stackexchange.com/questions/50109/bluetooth-device-list
-# #  https://forums.raspberrypi.com/viewtopic.php?t=240492
-# #
-# # --> default-agent
-# # --> scan on
-# # bluetooth ID for Dad's JBL Flip 5 -->  D8:37:3B:63:1B:30
-# # --> pair D8:37:3B:63:1B:30
+cd ~/src/JukeBox/src
+sudo cp /home/pi/src/JukeBox/src/jukebox.service /etc/systemd/system/jukebox.service
+sudo chmod 644 /etc/systemd/system/jukebox.service
+sudo systemctl daemon-reload
+sudo systemctl enable jukebox.service
+sudo systemctl start jukebox.service
